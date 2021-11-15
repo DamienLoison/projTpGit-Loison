207e63bd12e73a27dd625205dd2889959d597600
damien.loison.pro@gmail.com <test@debian>
2021-11-15 16:11:49 +0100

    developpement.php

---
+++ developpement2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'Présentation : ceci est la création de Loison';
+        ?>
+    </body>
+</html>
